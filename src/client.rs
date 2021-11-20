5828c37d82f6e66dde15ee774fb61939696c6347
Andrew Kvapil <viluon@pm.me>
2021-11-20 11:41:41 +0100

    Implement the client and the server

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,21 @@
 
+use sum_service::sum_service_client::SumServiceClient;
+use sum_service::SumRequest;
+
 pub mod sum_service {
     tonic::include_proto!("me.viluon.sum");
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = SumServiceClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(SumRequest {
+        numbers: vec![1, 2, 3],
+    });
+
+    let response = client.sum(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,43 @@
 
+use tonic::{transport::Server, Request, Response, Status};
+
+use sum_service::sum_service_server::{SumService, SumServiceServer};
+use sum_service::{SumRequest, SumResponse};
+
 pub mod sum_service {
     tonic::include_proto!("me.viluon.sum");
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Default)]
+pub struct SumServiceImpl {}
+
+#[tonic::async_trait]
+impl SumService for SumServiceImpl {
+    async fn sum(
+        &self,
+        request: Request<SumRequest>,
+    ) -> Result<Response<SumResponse>, Status> {
+        println!("SumServiceImpl::sum()");
+        println!("SumRequest: {:?}", request);
+        let sum_request = request.into_inner();
+        let sum_response = SumResponse {
+            sum: sum_request.numbers.iter().sum(),
+        };
+        println!("SumResponse: {:?}", sum_response);
+        Ok(Response::new(sum_response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let sum_service = SumServiceImpl::default();
+
+    println!("SumService listening on {}", addr);
+    Server::builder()
+        .add_service(SumServiceServer::new(sum_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
